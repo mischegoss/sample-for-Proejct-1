1cab60bea6587d2ac3ea3a0502dfa445f03d493f
Tamar <mischegoss@gmail.com>
2020-07-08 22:17:19 -0400

    Add DOM manipulation

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 // Test to make sure JS file is working
 
 console.log("This is working");
+
+//Grabbing DOM elements
+var domElements = ["name", "location", "forecast", "quote", "news", "picture"];
+
+for (i = 0; i < domElements.length; i++) {
+    domElements[i] = document.getElementById(domElements[i] + "-input")
+    console.log( domElements[i])
+}
+
+
+
