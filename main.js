f6f32430018e1309b619b1481b7b5180598c8e2c
Tamar <mischegoss@gmail.com>
2020-07-09 00:23:32 -0400

    Add API and image

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
+let currentLocation;
+let currentCondition;
+
 // Test to make sure JS file is working
 
 console.log("This is working");
@@ -13,6 +17,25 @@ for (i = 0; i < domElements.length; i++) {
 // Getting location API using JQuery .getJSON()
 
 $.getJSON('https://ipapi.co/json/', function(data){
-  console.log(locationElement)
-  locationElement.textContent = data.city;
+  currentLocation = data.city
+  locationElement.textContent = currentLocation;
+  getForecast(currentLocation)
 })
+
+//getting Forecast using Weather API and changing the image to reflect the current conditions
+
+function getForecast() {
+
+fetch("http://api.openweathermap.org/data/2.5/weather?q=" + currentLocation + "&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    forecastElement.textContent = "The current temperature is " + data.main.temp + " °F";
+    currentCondition = data.weather[0].description
+    currentCondition.replace(/\s+/g, ',').toLowerCase();
+    pictureElement.src = "https://source.unsplash.com/featured/?" + currentCondition;
+    pictureElement.srcset = "https://source.unsplash.com/featured/?" + currentCondition;
+
+  })
+}
